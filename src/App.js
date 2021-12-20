433ff9155c3e88dff3c679a9e884abf0bb576da4
chrystalme <afam_ifeanyi@live.com>
2021-12-20 17:46:53 +0200

    Add routes to App

--- src/App.js
+++ src/App.js
@@ -2,7 +2,8 @@ import React from 'react';
 import {BrowserRouter, Routes, Route } from 'react-router-dom';
 import Header from './component/Header';
 import Home from './component/Home';
-import Movie from './component/MovieDetails'
+import Movie from './component/MovieDetails';
+import PageNotFound from './component/PageNotFound';
 
 
 function App() {
@@ -13,6 +14,7 @@ function App() {
     <Routes>
     <Route exact path="/" element={<Home />} />
     <Route exact path="/movie/:imdbId" element={<Movie />} />
+    <Route element={<PageNotFound />} />
     </Routes>
   </BrowserRouter>
     </div>
---
+++ src/component/Header/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div>
+     <h3 className='text-center text-indigo-400'>Header</h3> 
+    </div>
+  )
+}
+
+export default Header
